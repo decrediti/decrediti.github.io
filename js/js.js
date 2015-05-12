dc7c62061102490823a8b9d8ea8fee4377747cc2
decrediti <decrediti@gmail.com>
2015-05-12 16:51:37 +0400

    Site update

--- js/js.js
+++ js/js.js
@@ -12,11 +12,11 @@ $(window).scroll(function(){
   if (scrolled > 20) {
   	control++;
   	$("#header").stop().css({"height":"185px","position":"absolute","top":"0"});
-  	$("#header img").stop().animate({"height":"230px"},400);
-  	$(".lefter").stop().animate({"width":"75px"},400);
+  	$("#header img").stop().animate({"height":"170px"},400);
+  	$(".lefter").stop().animate({"width":"28px"},400);
   	$("#header span").stop().animate({"font-size":"3em","margin":"70px 30px"},900);
   	$("#header br").remove();
-  	$("#header a").stop().animate({"margin-top":"-31px"},400);
+  	$("#header a").stop().animate({"margin-top":"-24px"},400);
   	$("#content").stop().animate({"margin":"188px 0 0 0"},900);
   	if (control == 1) {
   	    $("body").animate({scrollTop: 0},100);
